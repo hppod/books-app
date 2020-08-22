f6d6ae44d712f678074f53a265431f8eb7a0e420
hppod <hppodgornik@gmail.com>
2020-08-22 11:54:49 -0300

    Implementando o componente Welcome

--- src/app/modules/welcome/welcome.module.ts
+++ src/app/modules/welcome/welcome.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { WelcomeRoutingModule } from './welcome-routing.module';
-import { WelcomeComponent } from './welcome.component';
+import { MatCardModule } from "@angular/material/card"
+import { MatButtonModule } from "@angular/material/button"
+
+import { WelcomeRoutingModule } from './welcome-routing.module'
+import { WelcomeComponent } from './welcome.component'
 
 
 @NgModule({
   declarations: [WelcomeComponent],
   imports: [
     CommonModule,
-    WelcomeRoutingModule
+    WelcomeRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class WelcomeModule { }
