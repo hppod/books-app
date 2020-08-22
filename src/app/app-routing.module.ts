395dbc7af4da0299996000cfe32067d65973c528
hppod <hppodgornik@gmail.com>
2020-08-21 22:34:37 -0300

    Criando o component books e criando as rotas para books

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
 
-const routes: Routes = []
+const routes: Routes = [
+    {
+        path: 'books',
+        loadChildren: () => import('./modules/books/books.module').then(module => module.BooksModule)
+    },
+    {
+        path: '',
+        redirectTo: '/books',
+        pathMatch: 'full'
+    }
+]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
