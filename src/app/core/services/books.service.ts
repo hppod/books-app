2d10ce6485d44f4eaad15ec0ba1ae33ce17ab289
hppod <hppodgornik@gmail.com>
2020-08-22 21:05:34 -0300

    Implementando o método findAll no serviço de Livros

---
+++ src/app/core/services/books.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpResponse } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { Livro } from './../models/book.model'
+import { API_URL } from "./../api"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  constructor(private http: HttpClient) { }
+
+  findAllBooks(): Observable<HttpResponse<Livro[]>> {
+    return this.http.get<Livro[]>(`${API_URL}/livro/listarTodos`, { observe: 'response' })
+  }
+
+}
