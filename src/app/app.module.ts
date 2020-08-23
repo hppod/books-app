863e5a59141e278fc244e1bfe8aac1fcfb7b90b7
hppod <hppodgornik@gmail.com>
2020-08-22 21:04:27 -0300

    Instalando o Ngx-Toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http'
+import { ToastrModule } from 'ngx-toastr'
 
 import { NavigationModule } from "./modules/navigation/navigation.module"
 
@@ -14,8 +16,10 @@ import { AppComponent } from './app.component'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
-    NavigationModule
+    NavigationModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
